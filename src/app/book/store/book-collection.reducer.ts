web-dave/angular-advanced-workshop-1

solve--ngrx-use-reducer

src/app/book/store/book-collection.reducer.ts
@@ -1,0 +1,12 @@
+import { createReducer, on } from '@ngrx/store';
+import { createBookOptimistic } from './book-collection.actions';
+import { BookCollectionSlice } from './book-collection.slice';
+
+const initial: BookCollectionSlice = {
+  entities: []
+};
+
+export const bookCollectionReducer = createReducer(
+  initial,
+  on(createBookOptimistic, (slice, { book }) => ({ ...slice, entities: [...slice.entities, book] }))
+);

src/app/book/store/book.feature.ts
@@ -1,0 +1,1 @@
+export const bookFeatureName = 'books';

ng-practice/state-management-28-10-22

src/app/book/store/book-collection.reducer.ts
@@ -1,0 +1,12 @@
+import { createReducer, on } from '@ngrx/store';
+import { createBookStart } from './book-collection.actions';
+import { BookCollectionSlice } from './book-collection.slice';
+
+const initial: BookCollectionSlice = {
+  entities: []
+};
+
+export const bookCollectionReducer = createReducer(
+  initial,
+  on(createBookStart, (slice, { book }) => ({ ...slice, entities: [...slice.entities, book] }))
+);

src/app/book/store/book.feature.ts
@@ -1,0 +1,1 @@
+export const bookFeatureName = 'book';